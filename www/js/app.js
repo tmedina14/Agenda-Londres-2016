39751da69f1d9e956b8db08dfa8ddd4477a61f3c
moreyo <mpueyo12@iesnumancia.cat>
2016-01-11 14:00:50 +0100

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -6,20 +6,35 @@ window.addEventListener('load', function() {
 	
 	*/
     $JSView.declareView({ 
-        viewA: {
-            url: '/viewA',
-            template: 'views/viewA.html',
-            controller: 'viewA'
+        inici: {
+            url: '/inici',
+            template: 'views/inici.html',
+            controller: 'inici'
         },
-        viewB: {
-            url: '/viewB',
-            template: 'views/viewB.html',
-            controller: 'viewB'
+        dia1: {
+            url: '/dia1',
+            template: 'views/dia1.html',
+            controller: 'dia1'
         },
-        viewC: {
-            url: '/viewC',
-            template: 'views/viewC.html',
-            controller: 'viewC'
+        dia2: {
+            url: '/dia2',
+            template: 'views/dia2.html',
+            controller: 'dia2'
+        },
+        dia3: {
+            url: '/dia3',
+            template: 'views/dia3.html',
+            controller: 'dia3'
+        },
+        dia4: {
+            url: '/dia4',
+            template: 'views/dia4.html',
+            controller: 'dia4'
+        },
+        dia5: {
+            url: '/dia5',
+            template: 'views/dia5.html',
+            controller: 'dia5'
         }
     });
      
@@ -34,6 +49,6 @@ window.addEventListener('load', function() {
      
     /*Asign view start*/
     $JSView
-        .initView('viewA');
+        .initView('inici');
  
 }, false);
